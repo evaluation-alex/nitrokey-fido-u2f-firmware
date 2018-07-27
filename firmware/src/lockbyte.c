36c42b423e47fff4b050dbe9f012b23cb4e573ac
Szczepan Zalega <szczepan@nitrokey.com>
2018-07-27 18:45:30 +0200

    Make lockbyte value switchable
    Signed-off-by: Szczepan Zalega <szczepan@nitrokey.com>

--- firmware/src/lockbyte.c
+++ firmware/src/lockbyte.c
@@ -3,7 +3,11 @@
 #include <si_toolchain.h>
 #include "app.h"
 
-const uint8_t code lockbyte = 0x00;
+#ifdef _SECURE_EEPROM
+const uint8_t code lockbyte = 0x00; //lock read
+#else
+const uint8_t code lockbyte = 0xff; //default value
+#endif
 
 //#ifdef U2F_USING_BOOTLOADER
 //const uint8_t code BOOTSIGNB = 0xA5; //bootloader_signature_byte
